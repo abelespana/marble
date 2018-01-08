6311168986dea96489bb9276c0b6b4842533e6bf
Abel España (Mac) <abel.espana@me.com>
2018-01-08 17:55:33 +0100

    Probando JSON
    Probando

--- js/profile-interaction.js
+++ js/profile-interaction.js
@@ -1,11 +1,15 @@
-var userInfo = {
-  "name":"Abel",
-  "username":"abelespana",
-  "age":29,
-  "membersince":"29/11/2017",
-  "country":"Spain"
-};
 function ShowMyInfo() {
+  var abel = {
+    "name":"Abel",
+    "username":"abelespana",
+    "age":29,
+    "membersince":"29/11/2017",
+    "country":"Spain",
+    "favouritedessert":"tarta de queso"
+  };
+  // var abelJSON = ("js/abel.json");
+  // var abelJSONok = parseJSON("abelJSON");
+
   var name = document.getElementById("myName");
   var myNickname = document.getElementById("myNickname");
   var whenRegistered = document.getElementById("whenRegistered");
@@ -13,20 +17,20 @@ function ShowMyInfo() {
   var myCountry = document.getElementById("myCountry");
 
   name.style.display = "block";
-  name.innerHTML = userInfo.name;
+  name.innerHTML = abel.name;
+
   myNickname.style.display = "block";
+  myNickname.innerHTML = abel.username;
+
   whenRegistered.style.display = "block";
+  whenRegistered.innerHTML = abel.membersince;
+
   myAge.style.display = "block";
+  myAge.innerHTML = abel.age;
+
   myCountry.style.display = "block";
-
-
-
+  myCountry.innerHTML = abel.country;
 }
-
-
-
-
-
 
 // "skills":[
 //   "html",
