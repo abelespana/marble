4f704c3a712039723cfc448863b67e8f44952b52
Abel España (Mac) <abel.espana@me.com>
2018-01-09 17:29:56 +0100

    Done work
    Archivos JS, CSS modificado

--- js/profile-interaction.js
+++ js/profile-interaction.js
@@ -1,44 +1,65 @@
-function ShowMyInfo() {
-  var abel = {
-    "name":"Abel",
-    "username":"abelespana",
-    "age":29,
-    "membersince":"29/11/2017",
-    "country":"Spain",
-    "favouritedessert":"tarta de queso"
-  };
-  // var abelJSON = ("js/abel.json");
-  // var abelJSONok = parseJSON("abelJSON");
-
+//Función para rellenar el campo del usuario con datos del JSON
+function ShowUserInfo(username) {
   var name = document.getElementById("myName");
   var myNickname = document.getElementById("myNickname");
   var whenRegistered = document.getElementById("whenRegistered");
   var myAge = document.getElementById("myAge");
   var myCountry = document.getElementById("myCountry");
+  var modalWindow = document.getElementById("modalWindow");
+  var myJSON;
+
+  if (username === "blanca") {
+    myJSON = blanca;
+  }
+  else if (username === "abel") {
+    myJSON = abel;
+  }
+  else if (username === "mikel") {
+    myJSON = mikel;
+  }
+  else if (username === "rober") {
+    myJSON = rober;
+  }
+  else {
+    alert("No existe ese usuario");
+  }
 
   name.style.display = "block";
-  name.innerHTML = abel.name;
+  name.innerHTML += " " + myJSON.name;
 
   myNickname.style.display = "block";
-  myNickname.innerHTML = abel.username;
+  myNickname.innerHTML += " " + myJSON.username;
 
   whenRegistered.style.display = "block";
-  whenRegistered.innerHTML = abel.membersince;
+  whenRegistered.innerHTML += " " + myJSON.membersince;
 
   myAge.style.display = "block";
-  myAge.innerHTML = abel.age;
+  myAge.innerHTML += " " + myJSON.age;
 
   myCountry.style.display = "block";
-  myCountry.innerHTML = abel.country;
+  myCountry.innerHTML += " " + myJSON.country;
+
+  modalWindow.style.display = "none";
 }
 
-// "skills":[
-//   "html",
-//   "css",
-//   "javascript"
-// ]
+//Funciones para intercambiar datos entre vistas
+function ShowVoted() {
+  document.getElementById("barraSeparadora").style.display="block";
+  document.getElementById("profileUserVoted").style.display="block";
+  document.getElementById("profileUserVisited").style.display="none";
+  document.getElementById("profileUserComented").style.display="none";
 
-// console.log(userInfo.nombre);
-// console.log(objetoJSON.edad);
-// console.log(objetoJSON.skills);
-// console.log(objetoJSON.skills[1]);
+}
+function ShowVisited() {
+  document.getElementById("barraSeparadora").style.display="block";
+  document.getElementById("profileUserVoted").style.display="none";
+  document.getElementById("profileUserVisited").style.display="block";
+  document.getElementById("profileUserComented").style.display="none";
+}
+function ShowComented() {
+  document.getElementById("barraSeparadora").style.display="block";
+  document.getElementById("profileUserVoted").style.display="none";
+  document.getElementById("profileUserVisited").style.display="none";
+  document.getElementById("profileUserComented").style.display="block";
+}
+// Cosa a mejorar: se pueden hacer estos cambios con una sola función con condicionales para no repetir tanto código
