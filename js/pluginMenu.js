cf4432d1dbfeba9f2d27d026c4cfa1f43d1dd21b
Unknown <mcondezudaire@gmail.com>
2018-01-11 18:06:38 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ js/pluginMenu.js
@@ -0,0 +1,13 @@
+
+
+$( document ).ready( function(){
+
+   $(function() {
+    $(selector).pagination({
+        items: 100,
+        itemsOnPage: 1,
+        cssStyle: 'light-theme'
+    });
+});
+
+});
