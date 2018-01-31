b24915ec3f3b87a51286713aa05ffd9c4f9ffd4b
Blanchi <mononokeee@yahoo.es>
2018-01-31 17:51:26 +0100

    Auto stash before merge of "master" and "origin/master"
    Modal de usuario

--- js/modalRespond.js
+++ js/modalRespond.js
@@ -63,6 +63,41 @@ function cerrarEquis() {
 
 
 
+/*________________User Card Modal_______________*/ /*hay que revisar*/
+
+
+
+// Get the button that opens the modal
+/*var btn = document.getElementById("botonUserCard");*/
+
+/*var btn = document.getElementById("user11");*/
+
+var btn = document.getElementsByClassName("user111")[0];
+// Get the modal
+var modal = document.getElementById('userCard');
+
+console.log(modal);
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-userCard")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
 
 
