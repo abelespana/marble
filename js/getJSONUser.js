b4dbfe20a9c465999f98cafbe58216a5e91358af
Abel España (Mac) <abel.espana@me.com>
2018-02-15 09:41:43 +0100

    Promises OK

--- js/getJSONUser.js
+++ js/getJSONUser.js
@@ -1,74 +1,71 @@
 var idUsuario = window.sessionStorage.getItem("variable");
 
-// === DEFINICIÓN === 
-
-
-
-// ==== EJECUCIÓN ====
-
-//Comprobación patatera del usuario que estoy pasando, y qué hacer a continuación
-if (idUsuario == 1) {
-	console.log("Es Abel");
-	pedirUsuario("1", guardarUsuario);
-	$("#profilePic").attr("src", "http://abelespana.es/datos/profileabel.jpg");
-}
-else if (idUsuario == 2) {
-	console.log("Es Mikel");
-	pedirUsuario("2", guardarUsuario);
-	$("#profilePic").attr("src", "http://abelespana.es/datos/profilemikel.jpg");
-}
-else {
-	alert("No hay usuario");
-}
+// ==== DEFINICIÓN ====
+function guardarUsuario(rawData) {
+	window.sessionStorage.setItem("nombre", rawData.name);
+	window.sessionStorage.setItem("username", rawData.username);
+	window.sessionStorage.setItem("edad", rawData.age);
+	window.sessionStorage.setItem("pais", rawData.country);
+	window.sessionStorage.setItem("registrado", rawData.membersince);
+	window.sessionStorage.setItem("imagen", rawData.pic);
+	window.sessionStorage.setItem("postre", rawData.favouritedessert);
+} // Cierre definición función guardarUsuario
 
 // Inicio mi promesa
 var miPromesa = new Promise(function (resolve, reject) {
-   //Función para la petición Ajax
-	function pedirUsuario(id, funcionGuardadora) {
+
+	//Comprobación usuario
+	if (idUsuario == 1) {
+		console.log("Abel");
+		pedirUsuario("1");
+	}
+	else if (idUsuario == 2) {
+		console.log("Mikel");
+		pedirUsuario("2");
+	}
+	else {
+		alert("No hay usuario");
+	}
+	// Fin comprobación usuario
+	
+	// Lanzo petición Ajax
+	function pedirUsuario(id) {
 		$.ajax({
 			url: "http://www.abelespana.es/datos/usuario" + id + ".json",
 			method: "get",
 			dataType: "json",
 			async : "true",
 
-	// En caso de éxito
+	// En caso de éxito en la petición
 		success : function(datos){
-			var elUsuario = datos;
-			// console.log(elUsuario);
-			funcionGuardadora(elUsuario);
-			resolve();
-			console.log("la promesa está resuelta");
-			}, // Cierre gestión con éxito en la petición
+			guardarUsuario(datos); //Ejecuto función para guardar en sessionStorage
+			resolve(); // Y resuelvo la promesa
+		}, // Cierre gestión con éxito en la petición
 
 	// En caso de error
 		error : function(data,xhr,status){
-			console.log("Error. Datos" + data + " " + xhr.value + " " + status);
-			reject();
-			console.log("promesa rechazada");
+			console.log("Se ha producido el siguiente error: " + data + " " + xhr.value + " " + status);
+			reject(); // Y rechazo la promesa
 		} // Cierre gestión con error en la petición
 
 		}); // Cierre función jQuery Ajax
-	} // Cierre función que pide datos
-}); // Cierre de la promesa
+	} // Cierre función pedirUsuario
+}); // Cierre de la primera parte de la promesa
 
-miPromesa.then(function(){
-	console.log("éxito en la petición, motherfucker");
-   
-   function guardarUsuario(datos){
-		window.sessionStorage.setItem("nombre", datos.name);
-		window.sessionStorage.setItem("username", datos.username);
-		window.sessionStorage.setItem("edad",datos.age);
-		window.sessionStorage.setItem("pais",datos.country);
-		window.sessionStorage.setItem("registrado",datos.membersince);
-		window.sessionStorage.setItem("imagen", datos.pic);
-		window.sessionStorage.setItem("postre",datos.favouritedessert);
-	} 
+miPromesa.then(function(){ //Inicio función para pintar datos
+	$("#profilePic").attr("src", sessionStorage.imagen);
+	$("#myName").append(" " + sessionStorage.nombre + "<br>");
+	$("#myNickname").append(" " + sessionStorage.username +"<br>");
+	$("#myAge").append(" " + sessionStorage.edad + "<br>");
+	$("#myCountry").append(" " + sessionStorage.pais + "<br>");
+	$("#whenRegistered").append(" " + sessionStorage.registrado + "<br>");
+	$("#myName,#myNickname,#whenRegistered,#myAge,#myCountry").show();
 }); //Cierre del then de la promesa
 
-// Pintar los elementos. Comentado por no ser necesario una vez implantado la promesa (creo). 
-// $("#myName").append(" " + sessionStorage.nombre + "<br>");
-// $("#myNickname").append(" " + sessionStorage.username +"<br>");
-// $("#myAge").append(" " + sessionStorage.edad + "<br>");
-// $("#myCountry").append(" " + sessionStorage.pais + "<br>");
-// $("#whenRegistered").append(" " + sessionStorage.registrado + "<br>");
-// $("#myName,#myNickname,#whenRegistered,#myAge,#myCountry").show();
+
+
+
+
+
+
+
